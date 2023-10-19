a3bdaec1c34698d23e262980265aad658fff500a
Asim Mutasim <kroshtech@gmail.com>
2023-10-19 20:13:17 +0300

    Обнова

--- Labs/Lab4/Lab4.2.cpp
+++ Labs/Lab4/Lab4.2.cpp
@@ -1,87 +1,30 @@
-#include<math.h>
+// Вариант 20
+// Дана целочисленная прямоугольная матрица.Определить количество отрицательных элементов в тех строках,
+// Которые содержат хотя бы один нулевой элемент.
 #include<iostream>
-#include <bits/stdc++.h>
+#include<stdlib.h>
 using namespace std;
-int CountTrailingZeros(int n)
+int a[5][5];
+int main()
 {
-    // declare bitset of 64 bits
-    bitset<64> bit;
-
-    // set bitset with the value
-    bit |= n;
-
-    int zero = 0;
-
-    for (int i = 0; i < 64; i++) {
-        if (bit[i] == 0)
-            zero++;
-        // if '1' comes then break
-        else
-            break;
-    }
-
-    return zero;
-}
-
-int strongest_even(int n, int m) {
-    int x = m - n + 1;
-    int a[100000];
-    int counter{};
-    int max = 0;
-    int maximum = 0;
-    for (int i = 0; i < m - n + 1; i++) {
-        a[i] = n + i;
-        if (fmod(a[i], 2) != 0) continue;
-        while (fmod(a[i], 2) == 0) {
-            counter++;
-            a[i] /= 2;
-        }
-        if (counter > max) {
-            max = counter;
-            maximum = a[i] * pow(2, counter);
-
-        }
-        counter = 0;
-    }
-    return (maximum);
-}
-int count(int a) {
-
-}
-#include<math.h>
-#include <bits/stdc++.h>
-using namespace std;
-int CountTrailingZeros(int n)
-{
-    // declare bitset of 64 bits
-    bitset<64> bit;
-
-    // set bitset with the value
-    bit |= n;
-
-    int zero = 0;
-
-    for (int i = 0; i < 64; i++) {
-        if (bit[i] == 0)
-            zero++;
-        // if '1' comes then break
-        else
-            break;
-    }
-
-    return zero;
-}
-int strongest_even(int n, int m) {
-    int a[n - m + 1];
-    int index{};
-    int max = 0;
-
-    for (int i = 0; i < m - n + 1; i++) {
-        a[i] = CountTrailingZeros(n + i);
-        if (a[i] > max) {
-            max = a[i];
-            index = i;
-        }
-    }
-    return (n + index);
+	int a[5][5];
+	srand(time(NULL));
+	for (int i = 0; i < 5; i++) {
+		int counter = 0;
+		bool flag = false;
+		for (int j = 0; j < 5; j++) {
+			a[i][j] = 10-rand()%20;
+			cout << a[i][j] <<  " ";
+			if (a[i][j] < 0) {
+				counter++;
+			}
+			if (a[i][j] == 0) {
+				flag = true;
+			}
+		}
+		if (flag == true) {
+			cout << "	<---- number of negative numbers is : " << counter;
+		}
+		cout << endl;
+	}
 }
